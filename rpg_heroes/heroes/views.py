59b4d4262c53181d36957cfb1b1b9cf40da58d29
he1lhamster <vladhp@mail.ru>
2023-07-09 18:02:08 +0300

    minor clean code

--- rpg_heroes/heroes/views.py
+++ rpg_heroes/heroes/views.py
@@ -1,9 +1,6 @@
-from django.forms import model_to_dict
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import GenericViewSet, generics
-from rest_framework.response import Response
-from rest_framework import mixins
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from heroes.models import PCharacter, PClass
 from heroes.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from heroes.serializers import PCSerializer
@@ -18,50 +15,11 @@ class PCharacterAPIList(generics.ListCreateAPIView):
 class PCharacterAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = PCharacter.objects.all()
     serializer_class = PCSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication,)
 
 
 class PCharacterAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = PCharacter.objects.all()
     serializer_class = PCSerializer
     permission_classes = (IsAdminOrReadOnly, )
-
-
-# class PCharacterViewSet(mixins.CreateModelMixin,
-#                         mixins.RetrieveModelMixin,
-#                         mixins.UpdateModelMixin,
-#                         mixins.ListModelMixin,
-#                         GenericViewSet):
-#     queryset = PCharacter.objects.all()
-#     serializer_class = PCSerializer
-#
-#     def get_queryset(self):
-#         pk = self.kwargs.get('pk')
-#
-#         if not pk:
-#             return PCharacter.objects.all()[:3]
-#         return PCharacter.objects.filter(pk=pk)
-#
-#     @action(methods=['get'], detail=True)
-#     def p_class(self, request, pk=None): # ulr += /p_class
-#         p_class = PClass.objects.get(pk=pk)
-#         return Response({'classes': p_class.name})
-
-#
-# class PCharacterAPIList(generics.ListCreateAPIView):
-#     queryset = PCharacter.objects.all()
-#     serializer_class = PCSerializer
-#
-#
-# class PCharacterAPIUpdate(generics.UpdateAPIView):
-#     queryset = PCharacter.objects.all()
-#     serializer_class = PCSerializer
-#
-#
-# class PCharacterAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = PCharacter.objects.all()
-#     serializer_class = PCSerializer
-
-# class PCharactersAPIView(generics.ListAPIView):
-#     queryset = PCharacter.objects.all()
-#     serializer_class = PCSerializer
--- rpg_heroes/rpg_heroes/urls.py
+++ rpg_heroes/rpg_heroes/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from heroes.views import *
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/drf-auth/", include("rest_framework.urls")),
     path("api/v1/pcharacters/", PCharacterAPIList.as_view()),
     path("api/v1/pcharacters/<int:pk>/", PCharacterAPIUpdate.as_view()),
     path("api/v1/pcharactersdelete/<int:pk>", PCharacterAPIDestroy.as_view()),
+
+    path("api/v1/auth/", include("djoser.urls")),
+    re_path(r"^auth/", include("djoser.urls.authtoken")),
 ]
